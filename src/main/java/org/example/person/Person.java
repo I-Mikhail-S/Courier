34f5c3e2795b07426d18e760542ec19a49e3a93f
Stepan <stepantryaschin@mail.ru>
2023-04-05 17:22:39 +0300

    day 1

--- src/main/java/org/example/person/Person.java
+++ src/main/java/org/example/person/Person.java
@@ -1,15 +1,50 @@
 package org.example.person;
-
+/**
+ * Фундаментальный клас person....
+ */
 public abstract class Person {
 
-    private int id;
+    private int id;//
     private String name;
     private double speed;
     private double energy; //сила
+private String surName;
+
+/*    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
     }
 
     @Override
@@ -19,5 +54,5 @@ public abstract class Person {
                 ", name='" + name + '\'' +
                 ", speed=" + speed +
                 ", energy=" + energy + '}';
-    }
+    }*/
 }
