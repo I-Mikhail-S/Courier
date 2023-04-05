c099cc626ee43c68fc6b8e8be4e11d1d2de4cbdf
I-Mikhail-S <michaelivanchin@yandex.ru>
2023-04-05 15:55:58 +0300

    day one...master

---
+++ src/main/java/org/example/person/Courier.java
@@ -0,0 +1,7 @@
+package org.example.person;
+
+public class Courier extends Person{
+
+
+
+}
---
+++ src/main/java/org/example/person/Person.java
@@ -0,0 +1,51 @@
+package org.example.person;
+
+public abstract class Person {
+
+    private int id;
+    private String name;
+    private double speed;
+    private double energy; //сила
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", speed=" + speed +
+                ", energy=" + energy + '}';
+    }
+}
