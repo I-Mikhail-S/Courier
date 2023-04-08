ac1746d7e87c12b630175e2b28e55c6bf89ce661
Stepan <stepantryaschin@mail.ru>
2023-04-08 14:35:44 +0300

    Revert "day 3, rename classes"
    This reverts commit 952e1842b325cb9a9469f337b34add4dd5474fe1.
    
    # Conflicts:
    #	src/main/java/org/example/fabric/Builder.java
    #	src/main/java/org/example/main/Main.java

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -1,9 +1,13 @@
 package org.example.main;
 
-
+import org.example.enums.EnumCourier;
+import org.example.fabric.Builder;
+import org.example.person.Person;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Person car = new Builder(EnumCourier.CAR).build();
+        System.out.println(car.toString());
+        System.out.println(car.toString());
     }
 }
