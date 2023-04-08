04b0258ec36b06355eb2c1ccb520b25714aacf26
I-Mikhail-S <michaelivanchin@yandex.ru>
2023-04-08 14:56:17 +0300

    day 4, AgentCourier, remake BuilderOrder!

--- src/main/java/org/example/order/Order.java
+++ src/main/java/org/example/order/Order.java
@@ -6,49 +6,23 @@ import org.example.time.Time;
 // TODO: 08/04/2023
 public class Order {
     private int id;
-    private Point start;
-    private Point finish;
+    private Point pointStart;
+    private Point pointFinish;
     //private double distance;
     private Time timeStart;
     private Time timeFinish;
     private EnumOrder weight;
 
-    //private Courier? courier;
-
     public Order() {}
 
-    public Order(int id, Point start, Point finish, Time timeStart, Time timeFinish, EnumOrder weight) {
+    public Order(int id, Point pointStart, Point pointFinish, Time timeStart, Time timeFinish, EnumOrder weight) {
         this.id = id;
-        this.start = start;
-        this.finish = finish;
+        this.pointStart = pointStart;
+        this.pointFinish = pointFinish;
         this.timeStart = timeStart;
         this.timeFinish = timeFinish;
         this.weight = weight;
     }
-
-//Я думаю, что тут нужна проверка на актуальность, но я не уверен, как вы её делаете
-        //courier = courier;
-
-
-    /*
-
-
-    public Courier? getCourier() {
-    return courier;
-    }
-    public void setCourier(Courier? courier){
-    this.courier = courier
-    }
-
-    */
-    public EnumOrder getWeight() {
-        return weight;
-    }
-
-    public void setWeight(EnumOrder weight) {
-        this.weight = weight;
-    }
-
 
     public int getId() {
         return id;
@@ -58,20 +32,43 @@ public class Order {
         this.id = id;
     }
 
-    public Point getStart() {
-        return start;
+    public Point getPointStart() {
+        return pointStart;
     }
 
-    public void setStart(Point start) {
-        this.start = start;
+    public void setPointStart(Point pointStart) {
+        this.pointStart = pointStart;
     }
 
-    public Point getFinish() {
-        return finish;
+    public Point getPointFinish() {
+        return pointFinish;
     }
 
-    public void setFinish(Point finish) {
-        this.finish = finish;
+    public void setPointFinish(Point pointFinish) {
+        this.pointFinish = pointFinish;
     }
 
+    public Time getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(Time timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public Time getTimeFinish() {
+        return timeFinish;
+    }
+
+    public void setTimeFinish(Time timeFinish) {
+        this.timeFinish = timeFinish;
+    }
+
+    public EnumOrder getWeight() {
+        return weight;
+    }
+
+    public void setWeight(EnumOrder weight) {
+        this.weight = weight;
+    }
 }
